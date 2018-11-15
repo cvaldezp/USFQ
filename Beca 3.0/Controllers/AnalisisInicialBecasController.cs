2f5e954303ce420106f1760331ec1c352e4a5797
aaarmijoa <aaarmijoa@usfq.edu.ec>
2018-11-15 10:31:55 -0500

    Version 1.0 en producción

--- Beca 3.0/Controllers/AnalisisInicialBecasController.cs
+++ Beca 3.0/Controllers/AnalisisInicialBecasController.cs
@@ -22,7 +22,7 @@ namespace Beca_3._0.Controllers
             Asis_FinanEntities1 entities = new Asis_FinanEntities1();
 
 
-            //Validación si existe
+            //Validación si existe ya en la tabla
             var nameUser = User.Identity.Name;
 
             entities.PRC_SOLI_INICIAL_BECA3(nameUser);
